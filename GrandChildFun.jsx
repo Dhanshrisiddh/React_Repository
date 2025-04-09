792db33059d7c47a04591073cd94b41de420d524
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2025-04-09 11:19:17 +0530

    GrandChildFun

---
+++ GrandChildFun.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GrandChildFun = (props)=>{
+    return (
+
+        <>
+        <h1>Welcome to GrandChildFunc Component</h1>
+        <h2>Remaining Fee:{props.fee}</h2>
+
+        </>
+    )
+}
+export default GrandChildFun
