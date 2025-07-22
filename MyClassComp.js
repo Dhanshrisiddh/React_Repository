5779dc97231b0bde08a600dac49a5dc30b6ce283
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2025-07-22 05:36:38 +0530

    Add files via upload
    MyClassComp.js

---
+++ MyClassComp.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+class MyClassComp extends Component {
+    render(){
+
+    return(
+        <h2> Welcome to Class Component </h2>
+    );
+ }
+}
+export default MyClassComp;
+
+
+
+
+// import React, {Component} from "react";
+
+// class MyClassComp extends Component{
+//     render() {
+//         return(
+//             <h2>Hello</h2>
+//         );
+//     }
+// }
+// export default MyClassComp; 
